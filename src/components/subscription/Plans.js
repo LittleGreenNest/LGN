b34a601866129f4931e087e17ff2a13f8ab32cca
LittleGreenNest <hellolittlegreennest@gmail.com>
2025-08-02 13:08:15 +0800

    Fix: corrected App.css import path

--- src/components/subscription/Plans.js
+++ src/components/subscription/Plans.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import '../App.css';
+import '../../App.css';
 import WaitlistForm from '../components/WaitlistForm';
 import { supabase } from '../../supabaseClient';
 
