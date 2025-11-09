59fd3013c044079abcedb120a0d6a4ebd3799c26
LittleGreenNest <hellolittlegreennest@gmail.com>
2025-11-10 07:01:05 +0800

    fix: correct supabaseClient import path(s) in src/components/user

---
+++ build/static/js/488.b71b397c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklittle_green_nest=self.webpackChunklittle_green_nest||[]).push([[488],{2488:(e,t,n)=>{new Map,new Date}}]);
+//# sourceMappingURL=488.b71b397c.chunk.js.map
--- src/components/WaitlistForm.js
+++ src/components/WaitlistForm.js
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from 'react';
-import { supabase } from '../../supabaseClient';
+import { supabase } from '../supabaseClient';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
